58c026d4361ac6e14bcfc9f60fc8998d20c87178
Begothen <4y.madan.comrade@gmail.com>
2021-07-27 17:39:34 +0300

    123

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Grisha_1
 			int result = 0;
 			int operation = 0;
 
-
+			// loxloxloxloxlox
 			// Time to say that Grisha is completly lox
 			Console.WriteLine("You are lox, Grisha!");
 			// Greatings
