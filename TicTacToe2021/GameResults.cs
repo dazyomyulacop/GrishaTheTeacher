809b5f09afb75a240753e0dcb42505b7102fc709
dazyomyulacop <4y.madan.comrade@gmail.com>
2021-08-08 14:09:12 +0300

    TicTacToe
    Of course I forget about .gitignore

---
+++ TicTacToe2021/GameResults.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TicTacToe2021
+{
+    public struct GameResults
+    {
+        public void StreamResults()
+        {
+            string writePath = @"C:\Users\4ymad\source\repos\TicTacToe2021\Reults.txt";
+            string text = "Hello World";
+
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter(writePath, true, System.Text.Encoding.Default)) // без System.IO. НЕПОСРЕДСТВЕННО ПЕРЕД StreamWriter выдает ошибку.
+                                                                                                                          // Добавление перед классом вообще ничего не меняет
+            {
+                sw.WriteLine(text);
+            }
+        }
+    }
+}
